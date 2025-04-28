0ea057d4ad45829750223b2b12315ede5a067b6e
0xENDER <72353925+0xENDER@users.noreply.github.com>
2025-04-28 20:56:06 +0300

    Use `react-native-svg-app-icon` package to generate android logo assets

--- react-native.config.js
+++ react-native.config.js
@@ -1,18 +1,6 @@
 module.exports = {
     project: {
-        ios: {},
-        android: {}, // grouped into "project"
+        android: {}
     },
-    assets: ['./assets/images'], // Path to your icon image(s)
-    "react-native-asset": {
-        "path": "./assets/brand/green-logo.svg", // Path to your main icon file
-        "output": "images", // Optional output directory
-        "android": {
-            "icon": "mipmap",
-            "roundIcon": "mipmap-round"
-        },
-        "ios": {
-            "icon": "AppIcon"
-        }
-    }
+    assets: []
 };
